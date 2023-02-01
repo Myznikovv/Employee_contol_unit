48eb080d5d4c359d8c96a3e2c02656a0a2278ca6
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-02-01 11:29:29 +0300

    creating a component using data from the main component

--- src/components/employee-list/employee-list.jsx
+++ src/components/employee-list/employee-list.jsx
@@ -1,12 +1,17 @@
 import "./employee_list.css"
 import EmployeeCard from "../employee-card/employee-card";
 
-const EmployeeList = ()=>{
+const EmployeeList = ({data})=>{
+
+    let elements = data.map((item)=>{
+    const {id, ...itemValues}  = item;
+        return(
+            <EmployeeCard key={id} {...itemValues}/>
+        )
+    })
     return(
             <ul className="employee-list list-group">
-                <EmployeeCard/>
-                <EmployeeCard/>
-                <EmployeeCard/>
+                {elements}
             </ul>
 
     )
