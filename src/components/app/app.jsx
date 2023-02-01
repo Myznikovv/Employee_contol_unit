37ce1c7a44aa282fdf3c011d8743ada5788ee153
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-02-01 11:30:40 +0300

    creating a data object, simulation of using server information

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,16 @@ import './app.css'
 import AppInfo from "../app-info/app-info";
 import AppSearch from "../app-search/app-search";
 import AppPanel from "../app-panel/app-panel";
-import EmployeeCard from "../employee-card/employee-card";
 import EmployeeList from "../employee-list/employee-list";
 import AppAddEmployee from "../app-add-employee/app-add-employee";
 
 function App(){
+    const data = [
+        {name: "John", salary: 1000, increase:true, id:1},
+        {name: "Ivan", salary: 2000, increase:false, id:2},
+        {name: "Vitaly", salary: 5000, increase:true, id:3},
+        {name: "Artem", salary: 3000, increase:false, id:4}
+    ]
     return (
         <div className="app">
             <AppInfo/>
@@ -14,7 +19,7 @@ function App(){
                 <AppSearch/>
                 <AppPanel/>
             </div>
-            <EmployeeList/>
+            <EmployeeList data={data}/>
             <AppAddEmployee/>
         </div>
     );
